b7564cc816dbf2eddd077ca72e6330ab5d4acc70
Yuri Kovalov <yuri@yurikoval.com>
2013-06-18 19:13:07 +0900

    WIP

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,6 +5,21 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = nav
     @window.makeKeyAndVisible
+
+    @settings = NSUserDefaults.standardUserDefaults
+    ensure_credentials
     true
+  end
+
+  def ensure_credentials
+    if credentials = @settings.dictionaryForKey('credentials')
+    else
+      show_credentials_dialog
+    end
+  end
+
+  def show_credentials_dialog
+    alert = UIAlertView.alloc.initWithTitle "NoCredentials"
+    alert.show
   end
 end
